df1362667807748f87b7c587cead354021be3647
Joao <dev.joaogustavo.dev@gmail.com>
2024-03-01 21:09:47 -0300

    learned about processes and threads

---
+++ threading_and_multiprocessing.py
@@ -0,0 +1,13 @@
+#What is a process:
+"""
+A process is an instance of a program. 
+When we run a program, a process is created, then, a bounded part of RAM memory is used for dealing with the process.
+process they do not share any memory consume. For each process, it's reserved different spaces in the memory for each of them.
+"""
+
+#What is a thread?
+""""
+Threads, that can be translated as 'line', is a bounded part of some process.
+This means, when we have a sub-task in a process, we have a thread. 
+KEYWORDS: Microprocess, Sub-process, sub-systems, amount of instructions for executing a process.
+"""
